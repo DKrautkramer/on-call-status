a745bc3af2ea7f254c08ad484585af66fe928359
Derek Krautkramer <derek.krautkramer@gmail.com>
2017-02-04 20:20:46 -0600

    Updated root path to redirect to list

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import EditUsers from './components/editUsers/EditUsers.vue';
 import EditDepartments from './components/departments/EditDepartments.vue';
 
 export const routes = [
+    { path: '/', redirect: '/list'},
     { path: '/list', component: ActiveList },
     { path: '/users', component: EditUsers },
     { path: '/departments', component: EditDepartments }
