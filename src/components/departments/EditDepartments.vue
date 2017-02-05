263674e8d852d61af3da377aadb99dcd2caedf27
Derek Krautkramer <derek.krautkramer@gmail.com>
2017-02-05 11:49:58 -0600

    Added logic to display departments in a list and remove departments by Id

--- src/components/departments/EditDepartments.vue
+++ src/components/departments/EditDepartments.vue
@@ -1,5 +1,53 @@
 <template>
     <div>
-        This is edit departments component
+        <h3>Current Departments</h3>
+        <ul class="dept_list">
+            <li v-for="dept in departments">
+                {{ dept.departmentName }}
+                <span class="delete" @click="removeDept(dept.departmentId)">Delete</span>
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+    export default {
+        computed: {
+            departments() {
+               return this.$store.getters.departments;
+            }
+        },
+        methods: {
+            removeDept(id) {
+                let keepGoing = confirm('Are you sure you want to delete this department?');
+
+                if(keepGoing) {
+                    this.$store.dispatch('removeDept', id);
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .dept_list {
+        list-style: none;
+        padding: 0;
+    }
+    .dept_list li {
+        background: #fff;
+        box-sizing: border-box;
+        padding:5px 10px;
+    }
+    .dept_list li:nth-child(odd) {
+        background: #F0F0F0;
+    }
+    .delete {
+        color: red;
+        cursor: pointer;
+        float: right;
+    }
+    .delete:hover {
+        text-decoration: underline;
+    }
+</style>
--- src/store/modules/departments.js
+++ src/store/modules/departments.js
@@ -6,19 +6,41 @@ const state = {
 
 const mutations = {
     'SET_DEPT' (state, dept) {
+        localStorage.setItem('departments', JSON.stringify(dept));
         state.departments = dept;
+    },
+    'REMOVE_DEPT' (state, obj) {
+        let userCheck = false;
+
+        for(let i = 0; i < obj.allUsers.length; i++) {
+            if(obj.allUsers[i].departmentId === obj.id) {
+                userCheck = true;
+                break;
+            }
+        }
+
+        if(userCheck) {
+            alert('Cannot delete department if any users are currently assigned to this department.  Please assign users to a different department and try again.');
+        } else {
+            const idx = state.departments.map((o) => o.departmentId).indexOf(obj.id);
+            state.departments.splice(idx, 1);
+        }
     }
 };
 
 const actions = {
     initDeptData: ({commit}) => {
-        commit('SET_DEPT', departments);
+        if(JSON.parse(localStorage.getItem('departments'))) {
+            commit('SET_DEPT', JSON.parse(localStorage.getItem('departments')));
+        } else {
+            commit('SET_DEPT', departments);
+        }
     },
     addDept: ({commit}, userData) => {
 
     },
-    removeDept: ({commit}, id) => {
-
+    removeDept: ({state, commit, rootState}, id) => {
+        commit('REMOVE_DEPT', {id: id, allUsers: rootState.users.allUsers});
     }
 };
 
