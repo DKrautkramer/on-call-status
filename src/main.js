98dcb47a581581b7ddfbf049fe55bb38c834fc66
Derek Krautkramer <derek.krautkramer@gmail.com>
2017-02-04 20:17:45 -0600

    Updated routing logic again to use default hash tag to work better with github pages

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 
 const router = new VueRouter({
-    mode: 'history',
+    //mode: 'history',
     routes
 });
 
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,7 @@ import EditUsers from './components/editUsers/EditUsers.vue';
 import EditDepartments from './components/departments/EditDepartments.vue';
 
 export const routes = [
-    { path: '/', redirect: '/on-call-status/list'},
-    { path: '/on-call-status/', redirect: '/on-call-status/list'},
-    { path: '/on-call-status/list', component: ActiveList },
-    { path: '/on-call-status/users', component: EditUsers },
-    { path: '/on-call-status/departments', component: EditDepartments }
+    { path: '/list', component: ActiveList },
+    { path: '/users', component: EditUsers },
+    { path: '/departments', component: EditDepartments }
 ];
